23196931a92fb9b45a3dbb1ae4829c6b26a9936f
Liaksej <tarusov.liaksej@gmail.com>
2023-08-31 16:40:17 +0200

    Refactor code and update ECMAScript target version
    The '_fakerFabric.ts' was removed and its contents were moved to 'unread.ts' for better organization and readability. Additionally, an HTTP request file (hands.http) and an environment file (http-client.env.json) were added for easier testing and configuration, respectively. The rewrites rule in 'vercel.json' was updated to make the path dynamic. The ECMAScript target version in 'tsconfig.json' was also updated from ESNext to ES2022 for compatibility with updated JS features.

--- api/unread.ts
+++ api/unread.ts
@@ -1,6 +1,27 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import { generateData } from "./_fakerFabric";
+
+import { v4 as uuidv4 } from "uuid";
+import { faker } from "@faker-js/faker";
 
 export default function (request: VercelRequest, response: VercelResponse) {
   response.status(200).json(generateData());
+}
+
+function generateMessage() {
+  return {
+    id: uuidv4(),
+    from: faker.internet.email(),
+    subject: `Hello from ${faker.person.firstName()} ${faker.person.lastName()}!`,
+    body: `Long message body here`,
+    received: faker.date.past().getTime(),
+  };
+}
+
+function generateData() {
+  const messages = Array.from({ length: 10 }, generateMessage);
+  return {
+    status: "ok",
+    timestamp: Date.now(),
+    messages: messages,
+  };
 }
